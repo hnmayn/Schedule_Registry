74db352c994386fdc05ce7afa74ea39c1d5c1bef
ayano.homma <annin0408@gmail.com>
2023-02-28 00:18:04 +0900

    修正

--- src/main/java/constants/ForwardConst.java
+++ src/main/java/constants/ForwardConst.java
@@ -30,7 +30,7 @@ public enum ForwardConst {
     // jsp
     FW_ERR_UNKNOWN("error/unknown"),
     FW_TOP_INDEX("topPage/index"),
-    FE_LOGIN("login/login"),
+    FW_LOGIN("login/login"),
     FW_EMP_INDEX("employees/index"),
     FW_EMP_SHOW("employees/show"),
     FW_EMP_NEW("employees/new"),
