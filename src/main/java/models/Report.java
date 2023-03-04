b53074a99f5fe7ebc82c8c1440dd49c1527c9574
ayano.homma <annin0408@gmail.com>
2023-03-05 01:43:16 +0900

    修正

--- src/main/java/models/Report.java
+++ src/main/java/models/Report.java
@@ -29,7 +29,7 @@ import lombok.Setter;
 @NamedQueries({
     @NamedQuery(
             name = JpaConst.Q_REP_GET_ALL,
-            query = JpaConst.Q_EMP_GET_ALL_DEF),
+            query = JpaConst.Q_REP_GET_ALL_DEF),
     @NamedQuery(
             name = JpaConst.Q_REP_COUNT,
             query = JpaConst.Q_REP_COUNT_DEF),
